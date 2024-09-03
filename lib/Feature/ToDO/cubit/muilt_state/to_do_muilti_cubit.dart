aa3ca877224dfe89794bdca79d01c8ade3b7d6a9
unknown <abdowagih38@gmail.com>
2024-09-04 00:46:20 +0300

    create singel freeze

--- lib/Feature/ToDO/cubit/muilt_state/to_do_muilti_cubit.dart
+++ lib/Feature/ToDO/cubit/muilt_state/to_do_muilti_cubit.dart
@@ -1,34 +1,34 @@
-import 'package:bloc/bloc.dart';
-import 'package:qara/Feature/ToDO/cubit/muilt_state/to_do_muilti_state.dart';
+// import 'package:bloc/bloc.dart';
+// import 'package:qara/Feature/ToDO/cubit/muilt_state/to_do_muilti_state.dart';
 
-part 'to_do_multi_cubit.freezed.dart';
+// part 'to_do_multi_cubit.freezed.dart';
 
-class ToDoMultiCubit extends Cubit<ToDoMultiState> {
-  ToDoMultiCubit() : super(ToDoMultiState.initial());
+// class ToDoMultiCubit extends Cubit<ToDoMultiState> {
+//   ToDoMultiCubit() : super(ToDoMultiState.initial());
 
-  void addItem(String title, String description) {
-    if (state is _Loaded) {
-      final currentState = state as _Loaded;
-      if (currentState.items.length < 10) {
-        final updatedItems = List<Map<String, String>>.from(currentState.items)
-          ..add({'title': title, 'description': description});
-        emit(ToDoMultiState.loaded(updatedItems));
-      } else {
-        emit(ToDoMultiState.limitReached());
-      }
-    }
-  }
+//   void addItem(String title, String description) {
+//     if (state is _Loaded) {
+//       final currentState = state as _Loaded;
+//       if (currentState.items.length < 10) {
+//         final updatedItems = List<Map<String, String>>.from(currentState.items)
+//           ..add({'title': title, 'description': description});
+//         emit(ToDoMultiState.loaded(updatedItems));
+//       } else {
+//         emit(ToDoMultiState.limitReached());
+//       }
+//     }
+//   }
 
-  void deleteItem(int index) {
-    if (state is _Loaded) {
-      final currentState = state as _Loaded;
-      final updatedItems = List<Map<String, String>>.from(currentState.items)
-        ..removeAt(index);
-      emit(ToDoMultiState.loaded(updatedItems));
-    }
-  }
+//   void deleteItem(int index) {
+//     if (state is _Loaded) {
+//       final currentState = state as _Loaded;
+//       final updatedItems = List<Map<String, String>>.from(currentState.items)
+//         ..removeAt(index);
+//       emit(ToDoMultiState.loaded(updatedItems));
+//     }
+//   }
 
-  void loadItems(List<Map<String, String>> items) {
-    emit(ToDoMultiState.loaded(items));
-  }
-}
+//   void loadItems(List<Map<String, String>> items) {
+//     emit(ToDoMultiState.loaded(items));
+//   }
+// }
