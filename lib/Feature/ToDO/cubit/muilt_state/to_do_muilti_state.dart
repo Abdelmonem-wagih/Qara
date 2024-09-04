ac28ac29b81d99469caf14acd30a7a7aa2d84ab6
unknown <abdowagih38@gmail.com>
2024-09-04 11:12:47 +0300

    create multi freeze

--- lib/Feature/ToDO/cubit/muilt_state/to_do_muilti_state.dart
+++ lib/Feature/ToDO/cubit/muilt_state/to_do_muilti_state.dart
@@ -6,6 +6,7 @@ part 'to_do_multi_state.freezed.dart'; // Ensure this matches the generated file
 class ToDoMultiState with _$ToDoMultiState {
   const factory ToDoMultiState.initial() = _Initial;
   const factory ToDoMultiState.loading() = _Loading;
-  const factory ToDoMultiState.loaded(List<Map<String, String>> items) = _Loaded;
+  const factory ToDoMultiState.loaded(List<Map<String, String>> items) =
+      _Loaded;
   const factory ToDoMultiState.limitReached() = _LimitReached;
 }
